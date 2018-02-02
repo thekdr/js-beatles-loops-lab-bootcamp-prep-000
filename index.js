88af15fb1919b989108aed442e25771247265836
Kristopher Ross <thekdr@yahoo.com>
2018-02-02 17:49:14 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,13 @@
+function theBeatlesPlay(musicians, instruments) {
+  var arr = [];
+  for(var i = 0; i < musicians.length; i++) {
+    arr.push(`${musicians[i]} plays ${instruments[i]}`)
+  }
+  return arr;
+}
+
+function johnLennonFacts(facts) {
+  while(i < facts.lenth) {
+    
+  }
+}
